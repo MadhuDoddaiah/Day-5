7cb84bc33a2dce395cabd4cad9dd949288bb62d0
MadhuDoddaiah <ydmadhusudhan@gmail.com>
2022-07-26 02:33:48 +0530

    added_Num-to-wordcase_Program_file

---
+++ AC-10-Num-to-wordcase.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+echo "Enter number";
+read Number
+
+case $Number in
+		1)
+		echo "The number is Zero"
+		;;
+		2)
+		echo "The number is  One"
+		;;
+		3)
+		echo "The number is Two"
+		;;
+		4)
+                echo "The number is Four"
+                ;;
+                5)
+                echo "The number is Five"
+                ;;
+                6)
+                echo "The number is Six"
+                ;;
+		7)
+                echo "The number is Seven"
+                ;;
+                8)
+                echo "The number is Eight"
+                ;;
+                9)
+                echo "The number is Nine"
+                ;;
+		*)
+		echo "End of Code"
+		;;
+esac
+
+
